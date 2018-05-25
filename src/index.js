649243adbc2a915fb2ccbe6b12cfe76c626d2209
HM Nayem <hm.nayem.95@gmail.com>
2018-05-26 00:08:36 +0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './index.css'
+
+import $ from 'jquery'
+import popper from 'popper.js'
+import bootstrap from 'bootstrap'
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+}
